45311413f989bf9bf44edc9473ded0933440f392
harshgoyal0121 <73306683+harshgoyal0121@users.noreply.github.com>
2023-02-20 18:20:44 +0530

    Added solution - LeetHub

--- Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
@@ -92,11 +92,11 @@ Node* sortedMerge(Node* head1, Node* head2)
     Node *temp=New;
     while(head1 && head2){
         if(head1->data <= head2->data){
-            temp->next=new Node(head1->data);
+            temp->next=head1;
             head1=head1->next;
         }
         else {
-            temp->next=new Node(head2->data);
+            temp->next=head2;
             head2=head2->next;
         }
         temp=temp->next;
