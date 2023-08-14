a7cce17841004bc9729d0509758cde54cb3affcc
harshgoyal0121 <73306683+harshgoyal0121@users.noreply.github.com>
2023-08-14 11:00:28 +0530

    Added solution - LeetHub

--- Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
+++ Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
@@ -21,24 +21,25 @@ int main()
 int longestSubstrDistinctChars (string s)
 {
     // your code here
+    if(s.size()==1) return 1;
     string str="";
-        int ans=0;
-        for(int i=0; i<s.size(); i++){
-            string x="";
-            x+=s[i];
-            if(str.find(x) == string::npos){
-                str.push_back(s[i]);
-            }
-            else{
-                if(ans<=str.size())  ans=str.size();
-                int  pos = str.find(s[i]);
-                string n_str = str.substr(pos+1);
-                str.clear();
-                str+=n_str;
-                str.push_back(s[i]);
-            }
+    int ans=0;
+    for(int i=0; i<s.size(); i++){
+        string x="";
+        x+=s[i];
+        if(str.find(x) == string::npos){
+            str.push_back(s[i]);
         }
-        int x =str.size();
-        ans = max(ans, x);
-        return ans;
+        else{
+            if(ans<=str.size())  ans=str.size();
+            int  pos = str.find(s[i]);
+            string n_str = str.substr(pos+1);
+            str.clear();
+            str+=n_str;
+            str.push_back(s[i]);
+        }
+    }
+    int x =str.size();
+    ans = max(ans, x);
+    return ans;
 }
